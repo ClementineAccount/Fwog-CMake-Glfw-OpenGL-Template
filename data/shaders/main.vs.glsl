68a519620bfc96997f2869d6f1a49d32cff7cef5
ClementineAccount <ClementineShamaney@gmail.com>
2023-06-16 15:37:32 +0800

    rewrites shader

--- data/shaders/main.vs.glsl
+++ data/shaders/main.vs.glsl
@@ -1,36 +1,32 @@
 #version 460 core
 
-layout (location = 0) in vec3 iPosition;
-layout (location = 1) in vec3 iNormal;
-layout (location = 2) in vec2 iUv;
-layout (location = 3) in vec4 iTangent;
+layout(location = 0) in vec3 a_pos;
+layout(location = 1) in vec3 a_normal;
+layout(location = 2) in vec2 a_uv;
 
-layout (location = 0) out vec2 oUvs;
-layout (location = 1) out flat uint oBaseColorIndex;
+layout(location = 0) out vec2 v_uv;
 
-layout (location = 0) uniform mat4 uProjection;
-layout (location = 1) uniform mat4 uView;
-
-struct ObjectData
+layout(binding = 0, std140) uniform UBO0
 {
-    uint transformIndex;
-    uint baseColorIndex;
-    uint normalIndex;
+  mat4 viewProj;
+  vec3 eyePos;
 };
 
-layout (binding = 0) buffer BObjectData
+layout(binding = 1, std140) uniform UBO1
 {
-    ObjectData[] objectData;
-};
-
-layout (binding = 1) buffer BTransforms
-{
-    mat4[] transforms;
+  mat4 model;
+  vec4 color;
 };
 
 void main()
 {
-    oUvs = iUv;
-    oBaseColorIndex = objectData[gl_DrawID].baseColorIndex;
-    gl_Position = uProjection * uView * transforms[objectData[gl_DrawID].transformIndex] * vec4(iPosition, 1.0);
+  v_position =  (model * vec4(a_pos, 1.0)).xyz;
+  gl_Position =  viewProj * model * vec4(a_pos, 1.0);
+  o_color = color;
+  v_uv = a_uv;
+
+  mat3 normalMatrix = inverse(transpose(mat3(model)));
+
+  v_normal = normalize(normalMatrix * a_normal);
+  v_eye = eyePos;
 }
